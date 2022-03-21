15e8e6de177d936ce488d76be95d1e7a40b6f424
dpatrinos <demetrios.patrinos@gmail.com>
2022-03-21 19:27:38 -0400

    data fixes

--- bert/bert_test_execution.cmd
+++ bert/bert_test_execution.cmd
@@ -8,8 +8,8 @@ python run_classifier.py
 --task_name=cola 
 --do_predict=true 
 --data_dir=./data 
---vocab_file=./uncased_L-12_H-768_A-12/vocab.txt 
---bert_config_file=/uncased_L-12_H-768_A-12/bert_config.json 
+--vocab_file=./cased_L-12_H-768_A-12/vocab.txt 
+--bert_config_file=/cased_L-12_H-768_A-12/bert_config.json 
 --init_checkpoint=$TRAINED_MODEL_CKPT 
 --max_seq_length=128 
 --output_dir=./bert_output
--- bert/bert_train_execution.cmd
+++ bert/bert_train_execution.cmd
@@ -4,12 +4,12 @@ python run_classifier.py
 --do_eval=true 
 --do_predict=false 
 --data_dir=./data/ 
---vocab_file=./uncased_L-12_H-768_A-12/vocab.txt 
---bert_config_file=./uncased_L-12_H-768_A-12/bert_config.json 
---init_checkpoint=./uncased_L-12_H-768_A-12/bert_model.ckpt 
+--vocab_file=./cased_L-12_H-768_A-12/vocab.txt 
+--bert_config_file=./cased_L-12_H-768_A-12/bert_config.json 
+--init_checkpoint=./cased_L-12_H-768_A-12/bert_model.ckpt 
 --max_seq_length=128 
 --train_batch_size=32 
 --learning_rate=2e-5 
 --num_train_epochs=3.0 
 --output_dir=./bert_output/ 
---do_lower_case=True
+--do_lower_case=False
