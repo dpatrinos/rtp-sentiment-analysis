57e08920bf203a14b78a6f2414f0c30a70becff0
dpatrinos <demetrios.patrinos@gmail.com>
2022-03-22 11:09:53 -0400

    modified header

--- bert/run_classifier.py
+++ bert/run_classifier.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# THIS FILE HAS BEEN MODIFIED FOR THE PURPOSE OF THIS PROJECT
 """BERT finetuning runner."""
 
 from __future__ import absolute_import
